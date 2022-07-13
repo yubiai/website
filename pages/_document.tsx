65f4b60ef657bb74073135430f40d38a7d2b93fa
Lea <dev.venezia@gmail.com>
2022-07-13 16:43:34 +0100

    Config google analytics.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,30 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default class Document extends NextDocument {
   render() {
     return (
-      <Html lang='en'>
-        <Head />
+      <Html lang="en">
+        <Head>
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-VTC7MC331V"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-VTC7MC331V');
+        `}
+          </Script>
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
