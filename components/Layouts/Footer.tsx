fcd46108d81e64648c0eb1f884b5c6d231335122
nachobr <ignbritos@gmail.com>
2023-07-30 19:19:10 -0300

    fix: index responsive

--- components/Layouts/Footer.tsx
+++ components/Layouts/Footer.tsx
@@ -52,191 +52,217 @@ const Footer = () => {
   const isBaseOrMd = useBreakpointValue({ base: true, md: true, lg: true, xl: false });
   return (
     <>
-      <Flex>
+      <Flex alignItems={"center"}
+        justifyContent={"center"}>
         <>
           {isBaseOrMd ? (
-            <Flex
-              border={"1px dotted pink"}
-              alignItems={"flex-start"}
-              flexDirection={"row"}
-              justifyContent={"center"}
-              w={"100%"}
-              maxW={"100%"}>
-              <Flex
-                flexDirection={"column"}
-                as={Stack}
-                py={4}
-                w={"100%"}
-                maxW={`180px`}
-                spacing={2}
-                justify={"center"}
-                align={"flex-start"}
-              //border={"1px dotted blue"}
+            <>
+              <Stack
+                
+                alignItems={"center"}
+                justifyContent={"center"}
               >
-                <Stack
-
-                  direction={"column"} alignItems={"center"}>
-                  <Text color={"#0D47A1"} fontSize={"18px"} fontWeight={"700"}>ABOUT YUBIAI</Text>
-                  <Link href="/">About Us</Link>
-                  <Link href="/ecosystem">Team</Link>
-                  <Link href="https://medium.com/@yubiai.blog" target={"_blank"}>Blog</Link>
-                </Stack>
-              </Flex>
-              <Box
-                border={"1px dotted red"}
-                w={"100%"}
-                maxW={"200px"}
-              >
-                <Container
-                  as={Stack}
+                <Flex alignItems={"flex-start"}
+                  flexDirection={"row"}
+                  justifyContent={"center"}
                   w={"100%"}
-                  maxW={"170px"}
-                  py={4}
-                  direction={{ base: "column", md: "row", lg: "column" }}
-                  spacing={2}
-                  //justify={{ base: "center", md: "space-between" }}
-                  align={{ base: "center", md: "center" }}
-                >
-                  <Text color={"#0D47A1"} fontSize={"18px"} fontWeight={"700"}>RESOURCES</Text>
-                  <Link
-                    href={"/static/yubiai_whitepaper.pdf"}
-                    passHref legacyBehavior
-                  >
-                    White Paper
-                  </Link>
-                  <Link
-                    href={"/static/yubiai_whitepaper.pdf"}
-                    passHref legacyBehavior
-                  >
-                    Yellow Paper
-                  </Link>
-                  <Link
-                    href={"https://github.com/yubiai"}
-                    target={"_blank"}
-                  >
-                    GitHub
-                  </Link>
-                </Container>
-              </Box>
-              <Flex
-                as={Stack}
-                w={"100%"}
-                maxW={"177px"}
-                h={"100%"}
-                border={"1px dotted green"}
-                //maxH={"125px"}
-                py={4}
-                spacing={2}
-                direction={{ base: "column", md: "row", lg: 'column' }}
-                //justify={{ base: "center", md: "space-between", lg: "" }}
-                align={{ base: "center", md: "center" }}
-              >
-                <Text color={"#0D47A1"} fontSize={"18px"} fontWeight={"700"}>SUPPORT</Text>
-                <Link href="/faq">FAQ</Link>
-                <Link
-                  href="mailto: contact@yubiai.market"
-                >
-                  Contact Us
-                </Link>
-                <Link
-                  href={"https://app.yubiai.market/help/terms-and-conditions"}
+                  maxW={"100%"}>
+                  <Flex
+                    flexDirection={"column"}
+                    as={Stack}
+                    py={4}
+                    w={"100%"}
+                    maxW={`180px`}
+                    spacing={2}
+                    justify={"center"}
+                    align={"flex-start"}
 
-                >
-                  Terms of Service
-                </Link>
-                <Link
-                  href={"/privacypolicy"}
+                  >
+                    <Stack
 
-                >
-                  Privacy Policy
-                </Link>
-              </Flex>
-              <Box
-              //border={"1px dotted red"}
-              >
-                <Container
-                  as={Stack}
-                  w={"100%"}
-                  maxW={"200px"}
-                  direction={{ base: "column", md: "row", lg: "column" }}
-                  py={4}
-                  spacing={2}
-                  justify={{ base: "center", md: "space-between", lg: "space-between" }}
-                  align={{ base: "center", md: "center" }}
-
-                >
-                  <Text color={"#0D47A1"} fontSize={"18px"} fontWeight={"700"}>OUR COMMUNITY</Text>
-                  <Container
-                    display={"flex"}
-                    flexDirection={"row"}
-                    //border={"1px dotted black"}
-                    justifyContent={"space-between"}>
-                    <Stack direction={"column"} spacing={2}>
-                      <SocialButton label={"Telegram"} href={"https://t.me/yubiai"}>
-                        <FaTelegramPlane fontSize={45} color={"#0D47A1"} />
-                      </SocialButton>
-                      <SocialButton label={"Linktree"} href={"https://linktr.ee/Yubiai"}>
-                        <SiLinktree fontSize={45} color={"#0D47A1"} />
-                      </SocialButton>
-                      <SocialButton
-                        label={"Twitter"}
+                      direction={"column"} alignItems={"center"}>
+                      <Text color={"#0D47A1"} fontSize={"18px"} fontWeight={"700"}
+                        textAlign="center">ABOUT YUBIAI</Text>
+                      <Link href="/">About Us</Link>
+                      <Link href="/ecosystem">Team</Link>
+                      <Link href="https://medium.com/@yubiai.blog" target={"_blank"}>Blog</Link>
+                    </Stack>
+                  </Flex>
+                  <Box
+                    w={"100%"}
+                    maxW={"200px"}
+                  >
+                    <Container
+                      as={Stack}
+                      w={"100%"}
+                      maxW={"170px"}
+                      py={4}
+                      direction={{ base: "column", md: "row", lg: "column" }}
+                      spacing={2}
+                      //justify={{ base: "center", md: "space-between" }}
+                      align={{ base: "center", md: "center" }}
+                    >
+                      <Text
+                        display={"flex"}
+                        height={"55px"}
+                        alignItems={"center"}
+                        color={"#0D47A1"}
+                        fontSize={"18px"}
+                        fontWeight={"700"}>RESOURCES</Text>
+                      <Link
+                        href={"/static/yubiai_whitepaper.pdf"}
+                        passHref legacyBehavior
+                      >
+                        White Paper
+                      </Link>
+                      <Link
+                        href={"/static/yubiai_whitepaper.pdf"}
+                        passHref legacyBehavior
+                      >
+                        Yellow Paper
+                      </Link>
+                      <Link
                         href={"https://github.com/yubiai"}
+                        target={"_blank"}
                       >
-                        <FaGithub fontSize={45} color={"#0D47A1"} />
-                      </SocialButton>
-                    </Stack>
-                    <Stack direction={"column"} spacing={2}>
-                      <SocialButton
-                        label={"Discord"}
-                        href={"https://discord.gg/a9CQKJXb8X"}
-                      >
-                        <FaDiscord fontSize={45} color={"#0D47A1"} />
-                      </SocialButton>
-                      <SocialButton
-                        label={"Twitter"}
-                        href={"https://twitter.com/YubiaiM"}
-                      >
-                        <FaYoutube fontSize={45} color={"#0D47A1"} />
-                      </SocialButton>
-                      <SocialButton
-                        label={"YouTube"}
-                        href={"https://www.youtube.com/channel/UCbxVCRRIO3xGnZuPywJ_0TA"}
-                      >
-                        <FaYoutube fontSize={45} color={"#0D47A1"} />
-                      </SocialButton>
-                    </Stack>
-                  </Container>
-                </Container>
-              </Box>
-              <Flex
-                flexDirection={"column"}
-                ///position="relative"
-                as={Stack}
-                py={4}
-                h={"100%"}
-                maxH={"350px"}
-                w={"100%"}
-                maxW={`325px`}
-                spacing={2}
-                justify={"center"}
-                align={"center"}
-              //border={"1px dotted blue"}
-              >
-                <LogoYblue />
-                <Stack direction={"column"} spacing={2}>
-                  <Text><span style={{ color: "#0D47A1" }}>Yubiai </span>-  the first decentralized marketplace for secure transactions where you can buy & sell services,
-                    digital products and physical ones using crypto.</Text>
-                  <Text color="black">
-                    © {new Date().getFullYear()} Yubiai Marketplace. All rights reserved
-                  </Text>
-                </Stack>
-              </Flex>
-            </Flex>
+                        GitHub
+                      </Link>
+                    </Container>
+                  </Box>
+                  <Flex
+                    as={Stack}
+                    w={"100%"}
+                    maxW={"177px"}
+                    h={"100%"}
+
+                    //maxH={"125px"}
+                    py={4}
+                    spacing={2}
+                    direction={{ base: "column", md: "row", lg: 'column' }}
+                    //justify={{ base: "center", md: "space-between", lg: "" }}
+                    align={{ base: "center", md: "center" }}
+                  >
+                    <Text
+                      display={"flex"}
+                      height={"55px"}
+                      alignItems={"center"}
+                      color={"#0D47A1"} fontSize={"18px"} fontWeight={"700"}>SUPPORT</Text>
+                    <Link href="/faq">FAQ</Link>
+                    <Link
+                      href="mailto: contact@yubiai.market"
+                    >
+                      Contact Us
+                    </Link>
+                    <Link
+                      href={"https://app.yubiai.market/help/terms-and-conditions"}
+
+                    >
+                      Terms of Service
+                    </Link>
+                    <Link
+                      href={"/privacypolicy"}
+
+                    >
+                      Privacy Policy
+                    </Link>
+                  </Flex>
+                </Flex>
+                <Flex alignItems={"flex-start"}
+                  flexDirection={"row"}
+                  justifyContent={"center"}
+                  w={"100%"}
+                  maxW={"100%"}>
+                  <Box
+
+                  >
+                    <Container
+                      as={Stack}
+                      w={"100%"}
+                      maxW={"200px"}
+                      direction={{ base: "column", md: "row", lg: "column" }}
+                      py={4}
+                      spacing={2}
+                      justify={{ base: "center", md: "space-between", lg: "space-between" }}
+                      align={{ base: "center", md: "center" }}
+
+                    >
+                      <Text color={"#0D47A1"} fontSize={"18px"} fontWeight={"700"}>OUR COMMUNITY</Text>
+                      <Container
+
+                        display={"flex"}
+                        flexDirection={"row"}
+
+                        justifyContent={"center"}>
+                        <Stack direction={"row"}
+                          spacing={3}
+                          mr={"1em"}>
+                          <SocialButton label={"Telegram"} href={"https://t.me/yubiai"}>
+                            <FaTelegramPlane fontSize={45} color={"#0D47A1"} />
+                          </SocialButton>
+                          <SocialButton label={"Linktree"} href={"https://linktr.ee/Yubiai"}>
+                            <SiLinktree fontSize={45} color={"#0D47A1"} />
+                          </SocialButton>
+                          <SocialButton
+                            label={"Github"}
+                            href={"https://github.com/yubiai"}
+                          >
+                            <FaGithub fontSize={45} color={"#0D47A1"} />
+                          </SocialButton>
+                        </Stack>
+                        <Stack direction={"row"} spacing={3}>
+                          <SocialButton
+                            label={"Discord"}
+                            href={"https://discord.gg/a9CQKJXb8X"}
+                          >
+                            <FaDiscord fontSize={45} color={"#0D47A1"} />
+                          </SocialButton>
+                          <SocialButton
+                            label={"Twitter"}
+                            href={"https://twitter.com/YubiaiM"}
+                          >
+                            <FaTwitter fontSize={45} color={"#0D47A1"} />
+                          </SocialButton>
+                          <SocialButton
+                            label={"YouTube"}
+                            href={"https://www.youtube.com/channel/UCbxVCRRIO3xGnZuPywJ_0TA"}
+                          >
+                            <FaYoutube fontSize={45} color={"#0D47A1"} />
+                          </SocialButton>
+                        </Stack>
+                      </Container>
+                    </Container>
+                  </Box>
+
+                </Flex>
+                <Flex
+                  flexDirection={"column"}
+                  ///position="relative"
+                  as={Stack}
+                  py={4}
+                  h={"100%"}
+                  maxH={"350px"}
+                  w={"100%"}
+                  maxW={`325px`}
+                  spacing={2}
+                  justify={"center"}
+                  align={"center"}
+
+                >
+                  <LogoYblue />
+                  <Stack direction={"column"} spacing={2}>
+                    <Text><span style={{ color: "#0D47A1" }}>Yubiai </span>-  the first decentralized marketplace for secure transactions where you can buy & sell services,
+                      digital products and physical ones using crypto.</Text>
+                    <Text color="black">
+                      © {new Date().getFullYear()} Yubiai Marketplace. All rights reserved
+                    </Text>
+                  </Stack>
+                </Flex>
+              </Stack>
+            </>
           ) : (
             <>
 
               <Flex
-                border={"1px dotted pink"}
                 alignItems={"flex-start"}
                 flexDirection={"row"}
                 justifyContent={"center"}
@@ -251,19 +277,22 @@ const Footer = () => {
                   spacing={2}
                   justify={"center"}
                   align={"flex-start"}
-                //border={"1px dotted blue"}
+
                 >
                   <Stack
 
                     direction={"column"} alignItems={"center"}>
-                    <Text color={"#0D47A1"} fontSize={"18px"} fontWeight={"700"}>ABOUT YUBIAI</Text>
+                    <Text
+                      color={"#0D47A1"}
+                      fontSize={"18px"}
+                      fontWeight={"700"}
+                      textAlign="center">ABOUT YUBIAI</Text>
                     <Link href="/">About Us</Link>
                     <Link href="/ecosystem">Team</Link>
                     <Link href="https://medium.com/@yubiai.blog" target={"_blank"}>Blog</Link>
                   </Stack>
                 </Flex>
                 <Box
-                  border={"1px dotted red"}
                   w={"100%"}
                   maxW={"200px"}
                 >
@@ -303,8 +332,6 @@ const Footer = () => {
                   w={"100%"}
                   maxW={"177px"}
                   h={"100%"}
-                  border={"1px dotted green"}
-                  //maxH={"125px"}
                   py={4}
                   spacing={2}
                   direction={{ base: "column", md: "row", lg: 'column' }}
@@ -332,7 +359,7 @@ const Footer = () => {
                   </Link>
                 </Flex>
                 <Box
-                //border={"1px dotted red"}
+
                 >
                   <Container
                     as={Stack}
@@ -349,7 +376,6 @@ const Footer = () => {
                     <Container
                       display={"flex"}
                       flexDirection={"row"}
-                      //border={"1px dotted black"}
                       justifyContent={"space-between"}>
                       <Stack direction={"column"} spacing={2}>
                         <SocialButton label={"Telegram"} href={"https://t.me/yubiai"}>
@@ -400,7 +426,7 @@ const Footer = () => {
                   spacing={2}
                   justify={"center"}
                   align={"center"}
-                //border={"1px dotted blue"}
+
                 >
                   <LogoYblue />
                   <Stack direction={"column"} spacing={2}>
