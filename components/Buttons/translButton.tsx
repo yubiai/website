1c623770c8635177d7c2524b906f38d5f7a363e2
nachobr <ignbritos@gmail.com>
2023-08-02 15:00:09 -0300

    feat: change color to lang icon

--- components/Buttons/translButton.tsx
+++ components/Buttons/translButton.tsx
@@ -1,5 +1,5 @@
 import { ChevronDownIcon } from '@chakra-ui/icons';
-import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import { Box, Button, Menu, MenuButton, MenuItem, MenuList, useColorModeValue } from '@chakra-ui/react';
 import useTranslation from 'next-translate/useTranslation';
 import React from 'react';
 import setLanguage from 'next-translate/setLanguage';
@@ -8,6 +8,8 @@ import Cookies from 'js-cookie';
 
 const LanguageChange: React.FC = () => {
   const { lang } = useTranslation('common');
+  const bg = useColorModeValue('gray.800', 'white');
+  const textColor = useColorModeValue('#00BCD4', 'gray.800');
 
   const changeLang = async (lng: string) => {
     await setLanguage(lng);
@@ -22,27 +24,33 @@ const LanguageChange: React.FC = () => {
   return (
     <Menu >
       <MenuButton
-        marginRight={{base:"0", md:"10px"}}
-        style={{border: "1px solid white", height:"40px"}}
+        marginRight={{ base: "0", md: "10px" }}
+        style={{ border: "1px solid white", height: "40px" }}
         as={Button}
         aria-label="language"
         rightIcon={<ChevronDownIcon />}
         backgroundColor="transparent"
         color="white"
         //ml="1em"
-        p={{ base:"6px", md: "1em" }}
+        p={{ base: "6px", md: "1em" }}
         _hover={{ bg: '#1C538A', color: 'gray.200' }}
         _expanded={{ bg: 'blue.400' }}
         _focus={{ boxShadow: 'outline' }}
-        
-        // isDisabled
+
+      // isDisabled
       >
         {lang ? lang.toUpperCase() : 'LANG'}
       </MenuButton>
-      <MenuList style={{ minWidth: "fit-content" }}>
-        {lang !== "en" && <MenuItem  onClick={() => changeLang('en')}>EN</MenuItem>}
+
+      <MenuList
+        style={{ minWidth: "fit-content" }}
+        bg={bg}
+        color={textColor}
+      >
+        {lang !== "en" && <MenuItem onClick={() => changeLang('en')}>EN</MenuItem>}
         {lang !== "es" && <MenuItem onClick={() => changeLang('es')}>ES</MenuItem>}
       </MenuList>
+
     </Menu>
   );
 };
